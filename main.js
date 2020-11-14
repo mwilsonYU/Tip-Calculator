9cbee9456340f72e63d8da6d93d168cd29106314
Marcus Wilson <mwilson@nyc.yearup.org>
2020-11-13 23:36:40 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let btn = documents.getElementById('btn');
+
+btn.addEventListener('click', function(){
+  let billAmount = document.getElementById('bill-amount').value;
+  let tipPercentage = document.getElementById('tip-percentage').value;
+
+  
+  let tipAmount = document.getElementById('tip_amount').value = billAmount / tipPercentage;
+  document.getElementById('total_bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+
+ })
