10d6a28f87b1bf93f33b52b56a5ba5d1f589c7d1
mwilsonYU <74393831+mwilsonYU@users.noreply.github.com>
2020-11-16 14:57:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let btn = document.getElementById('button');
+var btn = document.getElementById('button');
 
 btn.addEventListener('click', function(){
-  let billAmount = Number(document.getElementById('bill-amount').value);
-  let tipPercentage = Number(document.getElementById('tip-percentage').value);
+  var billAmount = Number(document.getElementById('bill_amount').value);
+  var tipPercentage = Number(document.getElementById('tip_percentage').value);
 
   
-  let tipAmount = document.getElementById('tip_amount').value = billAmount * tipPercentage;
+  var tipAmount = document.getElementById('tip_amount').value = billAmount * tipPercentage;
   document.getElementById('total_bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
 
  })
